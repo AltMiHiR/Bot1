691e5047cacf8a197f27110d8605fd78cd609089
⌁ ⏤͟͞ 𝙄𝙩𝙯𝙈𝙞𝙝𝙞𝙧™ / <mihirxd13@gmail.com>
2025-01-22 13:13:35 +0530

    Update m.py

--- m.py
+++ m.py
@@ -1,10 +1,10 @@
 import pytz
+import logging
 import asyncio
 import threading
-import logging
 
-from pyrogram import Client, idle
-from pyrogram.errors import UserAlreadyParticipant
+from pyrogram import Client, filters, idle
+from pyrogram.types import Message
 
 from datetime import datetime, timedelta
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -25,16 +25,15 @@ CHATS = {
     -1001170860302: "@MarketStrong",
     -1001527007389: "https://t.me/joinchat/TeqEqGaau7UyZTFk",
     -1001469840857: "https://t.me/joinchat/zRL6AlXSWtkzYTFk",
-    # -1001325622242: "https://t.me/joinchat/dNMbjY4oOwY4OGI0",
-    # -1001595987025: "https://t.me/joinchat/ybXSq6ZDVDcwNDg8",
-    # -1001493802984: "https://t.me/joinchat/SMPfqFkJn-gb_pZmMMvXJQ",
-    # -1001320290633: "https://t.me/joinchat/IWC4FxHDbeOVgyLntkEeaQ",
+    -1001325622242: "https://t.me/joinchat/dNMbjY4oOwY4OGI0",
+    -1001595987025: "https://t.me/joinchat/ybXSq6ZDVDcwNDg8",
+    -1001493802984: "https://t.me/joinchat/SMPfqFkJn-gb_pZmMMvXJQ",
+    -1001320290633: "https://t.me/joinchat/IWC4FxHDbeOVgyLntkEeaQ",
     # -1001153450526: "https://t.me/joinchat/J_aD1Jjfft8xNzNk",
     # -1001155030078: "https://t.me/joinchat/Kk-O3hRHsvRl-q_uuORacA",
     # -1001205313235: "https://t.me/joinchat/PmGvgh0v0sJkLoxBe0d3Rg",
     # -1001242480702: "https://t.me/joinchat/QcUw8BTnVbPRexXPUA1JlQ",
     # -1001214829860: "@CHRISTIANSEXCLUB",
-    # -1001192385192: "@NewGoodShiddD_Drugs",
     # -1001293195336: "@MADEITOUTTHASTONE",
     # -1001232993323: "@kofecktkassin",
     # -1001147510917: "https://t.me/joinchat/McFg_URloIV0FesEJDfAAw",
@@ -54,6 +53,16 @@ class ThreadSafeList(list):
     def extend(self, iterable):
         with self._lock:
             super().extend(iterable)
+
+    def copy_with_clear(self):
+        with self._lock:
+            data = super().copy()
+            super().clear()
+            return data
+
+    def copy(self):
+        with self._lock:
+            return super().copy()
 
     def remove(self, item):
         with self._lock:
@@ -92,26 +101,36 @@ class ThreadSafeList(list):
             super().__delitem__(index)
 
 
-class Userbot(Client):
-    async def start(self):
-        await super().start()
-        # try:
-        #     async for dialog in self.get_dialogs():
-        #         continue
-        # except:
-        #     pass
-
-
 DATA = ThreadSafeList()
+
+app = Client(ACCOUNT["phone_number"], session_string=ACCOUNT["session"], max_message_cache_size=0)
+
+@app.on_message(filters.group & filters.user([5961091462, 277756078, 756558173, 126203394, 108918720]))
+async def _tracker(client: Client, message: Message):
+    data = {
+        "user_id": message.from_user.id,
+        "chat_id": message.chat.id,
+        "message_id": message.id,
+        "timestamp": int(message.date.timestamp())
+    }
+    DATA.append(data)
 
 
 async def main():
-    app = Userbot(ACCOUNT["phone_number"], session_string=ACCOUNT["session"], max_message_cache_size=0)
-
     await app.start()
-    print("USERBOT STARTED!")
+    print("USERBOT STARTED!\n")
 
-    await idle()
+    while True:
+        await asyncio.sleep(300)
+
+        if len(DATA) > 0:
+            data = DATA.copy_with_clear()
+            await TrackDataCol.insert_many(data)
+            day = datetime.now(IST)
+            print(f"ADDED_{len(data)}_DOCS - {day.strftime('%d-%m-%Y %H:%M:%S')}")
+        else:
+            day = datetime.now(IST)
+            print(f"ADDED_0_DOCS - {day.strftime('%d-%m-%Y %H:%M:%S')}")
 
     await app.stop()
     print("USERBOT STOPPED!")
