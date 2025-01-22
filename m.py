e3128fd41f3ea77d57a93d2798d9d327fdab52d0
⌁ ⏤͟͞ 𝙄𝙩𝙯𝙈𝙞𝙝𝙞𝙧™ / <mihirxd13@gmail.com>
2025-01-22 12:31:51 +0530

    Update m.py

--- m.py
+++ m.py
@@ -3,7 +3,7 @@ import asyncio
 import threading
 import logging
 
-from pyrogram import Client
+from pyrogram import Client, idle
 from pyrogram.errors import UserAlreadyParticipant
 
 from datetime import datetime, timedelta
@@ -95,82 +95,23 @@ class ThreadSafeList(list):
 class Userbot(Client):
     async def start(self):
         await super().start()
-        try:
-            async for dialog in self.get_dialogs():
-                continue
-        except:
-            pass
+        # try:
+        #     async for dialog in self.get_dialogs():
+        #         continue
+        # except:
+        #     pass
 
 
 DATA = ThreadSafeList()
 
 
 async def main():
-    app = Userbot(ACCOUNT["phone_number"], session_string=ACCOUNT["session"], no_updates=True, max_message_cache_size=0)
+    app = Userbot(ACCOUNT["phone_number"], session_string=ACCOUNT["session"], max_message_cache_size=0)
 
     await app.start()
     print("USERBOT STARTED!")
 
-    is_not_first = False
-
-    while True:
-        if is_not_first:
-            current_date = datetime.now(IST)
-            next_day = current_date + timedelta(days=1)
-            next_day = next_day.replace(hour=0, minute=0, second=0, microsecond=0)
-            sleep_seconds = (next_day - current_date).total_seconds()
-            if sleep_seconds:
-                await asyncio.sleep(sleep_seconds)
-        else:
-            is_not_first = True
-
-        today = datetime.now(IST)
-        previous_day = today - timedelta(days=1)
-        today = today.astimezone(IST).replace(hour=0, minute=0, second=0, microsecond=0)
-        previous_day = previous_day.astimezone(IST).replace(hour=0, minute=0, second=0, microsecond=0)
-
-        for chat_id, chat_link in CHATS.items():
-            # Joining Chat if not Joined
-            try:
-                chat = await app.get_chat(chat_link)
-                await app.get_chat_member(chat_id=chat.id, user_id="me")
-            except:
-                try:
-                    chat = await app.join_chat(chat_link)
-                except UserAlreadyParticipant:
-                    try:
-                        chat = await app.get_chat(chat_link)
-                    except Exception as e:
-                        print(f"Failed to Access: {type(e).__name__} ( {chat_link} )")
-                        continue
-                except Exception as e:
-                    print(f"Failed to Join: {type(e).__name__} ( {chat_link} )")
-                    continue
-            print(f"RUNNING {chat_id}...  {datetime.now(IST).strftime('%d-%m-%Y %H:%M:%S')}", end=" ")
-            mcount = 0
-            try:
-                async for message in app.search_messages(chat_id=chat_id, min_date=previous_day, max_date=today):
-                    mcount += 1
-                    if mcount % 1000 == 0:
-                        await asyncio.sleep(3)
-                    if message.from_user and message.from_user.id in [5961091462, 277756078, 756558173, 126203394, 108918720]:
-                        data = {
-                            "user_id": message.from_user.id,
-                            "chat_id": message.chat.id,
-                            "message_id": message.id,
-                            "timestamp": int(message.date.timestamp())
-                        }
-                        DATA.append(data)
-            except Exception as e:
-                print(f"ERROR_{type(e).__name__}", end=" ")
-            if len(DATA) > 0:
-                await TrackDataCol.insert_many(DATA)
-                print(f"ADDED_{len(DATA)}_DOCS_OF_{previous_day.strftime('%d-%m-%Y')}")
-                DATA.clear()
-            else:
-                print(f"ADDED_0_DOCS_OF_{previous_day.strftime('%d-%m-%Y')}")
-            now = datetime.now()
-            await asyncio.sleep((60 - now.second) + ((59 - now.minute) * 60))
+    await idle()
 
     await app.stop()
     print("USERBOT STOPPED!")
